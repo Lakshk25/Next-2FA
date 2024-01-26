afe10d9c66c11be5333dd8f8b5c4b99c7c0af31d
Lakshk25 <raghavkumrawat01039@gmail.com>
2024-01-27 02:25:00 +0530

    domain name

--- lib/mail.ts
+++ lib/mail.ts
@@ -7,7 +7,7 @@ export const sendVerificationEmail = async(email:string, token:string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
     
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'mail@lakshweb.online',
         to: email,
         subject: "Confirm your email",
         html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
@@ -21,7 +21,7 @@ export const sendPasswordResetEmail = async (
     const resetLink = `${domain}/auth/new-password?token=${token}`
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@lakshweb.online",
         to: email,
         subject: "Reset your password",
         html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
@@ -30,7 +30,7 @@ export const sendPasswordResetEmail = async (
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@lakshweb.online",
         to: email,
         subject: "2 Factor Authentication Code",
         html: `<p>Your 2 Factor Authentication code: ${token}</p>`
